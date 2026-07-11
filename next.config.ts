ffcd884e4b460cc59ef0b02481676e91bc226dfb
Subash Singh Dhami <subashdhamee@gmail.com>
2026-07-11 13:52:07 +0545

    chore: initialize Next.js project

---
+++ next.config.ts
@@ -0,0 +1,20 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  poweredByHeader: false,
+  turbopack: {
+    root: process.cwd(),
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        pathname: "/aida-public/**",
+      },
+    ],
+    formats: ["image/avif", "image/webp"],
+  },
+};
+
+export default nextConfig;
